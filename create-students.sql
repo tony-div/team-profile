b7c1bae77e50b4d0e87ddc2cc4004bd3cd637ef2
tony-div <tg764562@gmail.com>
2024-05-06 19:02:21 +0300

    update db constraints

--- create-students.sql
+++ create-students.sql
@@ -1,6 +1,7 @@
 Create table Students(
 id int primary key,
 name varchar(70) not null,
-age int check (Age>0 AND Age<=100),
+age int check (Age>=16 AND Age<=100),
 CGPA Decimal(5,4) check (CGPA >= 0 AND CGPA <= 4.0)
 );
+
